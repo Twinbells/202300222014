083892d3a7246fb1587d22aefde5f2b011b7956a
Twinbells <167402767+Twinbells@users.noreply.github.com>
2024-06-04 12:06:59 +0800

    Create 2_1_sizeof.c

---
+++ 2_1_sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("The size of char is %zu bytes.\n", sizeof(char));
+    printf("The size of int is %zu bytes.\n", sizeof(int));
+    printf("The size of float is %zu bytes.\n", sizeof(float));
+    printf("The size of double is %zu bytes.\n", sizeof(double));
+    printf("The size of long is %zu bytes.\n", sizeof(long));
+    printf("The size of long long is %zu bytes.\n", sizeof(long long));
+    return 0;
+}
