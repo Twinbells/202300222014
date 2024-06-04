b5b166313b435607984d6ac77c24060cca851ab5
Twinbells <167402767+Twinbells@users.noreply.github.com>
2024-06-04 12:35:45 +0800

    Create 2_3_print_char_pattern.c

---
+++ 2_3_print_char_pattern.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void print_char_pattern(char c) {
+    const char *red = "\033[31m";
+    const char *reset = "\033[0m";
+
+    switch (c) {
+        case 'E':
+            printf("%s******\n", red);
+            printf("%s*\n", red);
+            printf("%s******\n", red);
+            printf("%s*\n", red);
+            printf("%s******\n", red);
+            printf("%s\n", reset);
+            break;
+        case 'A':
+            printf("%s   *   \n", red);
+            printf("%s  * *  \n", red);
+            printf("%s * * * \n", red);
+            printf("%s*     *\n", red);
+            printf("%s\n", reset);
+            break;
+        case 'H':
+            printf("%s*    *\n", red);
+            printf("%s*    *\n", red);
+            printf("%s*    *\n", red);
+            printf("%s******\n", red);
+            printf("%s*    *\n", red);
+            printf("%s*    *\n", red);
+            printf("%s\n", reset);
+            break;
+        default:
+            printf("Unsupported character for pattern output.\n");
+            break;
+    }
+}
+
+int main() {
+    char c;
+    printf("Enter an uppercase English letter (E, A, or H): ");
+    scanf(" %c", &c); 
+    print_char_pattern(c);
+
+    return 0;
+}
